9098fab436bd32abf3961ed1321266d35874c10f
lolstorm92 <ricardo.s.campos@tecnico.ulisboa.pt>
2020-01-31 11:36:20 +0000

    Tabs to spaces

--- Tangram2D/src/Figure.cpp
+++ Tangram2D/src/Figure.cpp
@@ -1,19 +1,19 @@
 #include "Figure.h"
 
 Figure::Figure() {
-	m_transform = cgj::MatrixFactory::CreateTransformMatrix(0.0f,0.0f,0.0f,0.0f,1.0f);
+    m_transform = cgj::MatrixFactory::CreateTransformMatrix(0.0f,0.0f,0.0f,0.0f,1.0f);
 }
 
 Figure::Figure(const cgj::Vector3& pos, float rotation, float scale) {
-	m_transform = cgj::MatrixFactory::CreateTransformMatrix(pos.x,pos.y,pos.z,rotation,scale);
+    m_transform = cgj::MatrixFactory::CreateTransformMatrix(pos.x,pos.y,pos.z,rotation,scale);
 }
 
 void Figure::addData(const cgj::ModelMesh& mesh) {
-	m_model.addData(mesh);
+    m_model.addData(mesh);
 }
 
 void Figure::draw(cgj::ShaderProgram& s) {
-	s.use();
+    s.use();
     m_model.bindVAO();
     GLint uniformID = s.getUniformLocation("ModelMatrix");
     glUniformMatrix4fv(uniformID,1,GL_FALSE, m_transform.data);
--- Tangram2D/src/Figure.h
+++ Tangram2D/src/Figure.h
@@ -4,14 +4,14 @@
 #include <CGJ/Maths.h>
 
 class Figure {
-	public:
-		Figure();
-		Figure(const cgj::Vector3& pos, float rotation, float scale);
-		~Figure() = default;
-		void draw(cgj::ShaderProgram& s);
-		void addData(const cgj::ModelMesh& mesh);
-	private:
-		cgj::Matrix4 m_transform;
-		cgj::Model m_model;	
+    public:
+        Figure();
+        Figure(const cgj::Vector3& pos, float rotation, float scale);
+        ~Figure() = default;
+        void draw(cgj::ShaderProgram& s);
+        void addData(const cgj::ModelMesh& mesh);
+    private:
+        cgj::Matrix4 m_transform;
+        cgj::Model m_model;    
 };
 #endif
